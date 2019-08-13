3eb38c368962609052a2ddb655353ef77a923768
Patryk Nowak <nowak.p92@gmail.com>
2019-08-13 22:04:19 +0200

    Add simple usage of class Graph in main()

--- graphs/src/main.cpp
+++ graphs/src/main.cpp
@@ -6,5 +6,28 @@ int main()
 {
     std::cout << "Hello world!" << std::endl;
 
+
+    Graph<> gr;
+
+    gr.addVertice(1);
+    gr.addVertice(100);
+    gr.addVertice(10);
+    gr.addVertice(20);
+
+    gr.addEdge(1, 100, 5);
+    gr.addEdge(100, 1, -5);
+
+    gr.addEdge(1, 1, 20);
+    gr.addEdge(1, 10, 123);
+    gr.addEdge(1, 10, 125);
+
+    for (const auto& v : gr)
+    {
+        std::cout << "gr value: " << v.first << std::endl;
+        for (const auto& e : v.second)
+        {
+            std::cout << " edge: " << v.first << " -> " << *e.first << ": " << e.second << std::endl;
+        }
+    }
 }
 
