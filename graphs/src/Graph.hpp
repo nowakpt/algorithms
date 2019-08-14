48dd6f103256a4f77063a63a761c1fa272172e12
Patryk Nowak <nowak.p92@gmail.com>
2019-08-14 15:51:26 +0200

    Add const_iterators and methods to delete vertices/edges from graph

--- graphs/src/Graph.hpp
+++ graphs/src/Graph.hpp
@@ -19,6 +19,22 @@ public:
         _vertices.emplace(std::forward<T>(value), edges_map_t{});
     }
 
+    void deleteVertice(const Tv& value)
+    {
+        auto toBeRemoved = _vertices.find(value);
+
+        if (toBeRemoved != _vertices.end())
+        {
+            for (auto& v : _vertices)
+            {
+                auto& edges = v.second;
+                edges.erase(&toBeRemoved->first);
+            }
+
+            _vertices.erase(toBeRemoved);
+        }
+    }
+
     template <typename T>
     void addEdge(const Tv& from, const Tv& to, T&& value)
     {
@@ -33,7 +49,28 @@ public:
         }
     }
 
+    void deleteEdge(const Tv& from, const Tv& to)
+    {
+        auto iterFrom = _vertices.find(from);
+        auto iterTo = _vertices.find(to);
+
+        if (iterFrom != _vertices.end() &&
+            iterTo != _vertices.end())
+        {
+            auto& edges = iterFrom->second;
+            auto toBeRemoved = edges.find(&iterTo->first);
+
+            if (toBeRemoved != edges.end())
+            {
+                edges.erase(toBeRemoved);
+            }
+        }
+    }
+
     // iterators
+    typename vertices_map_t::const_iterator begin() const { return _vertices.begin(); }
+    typename vertices_map_t::const_iterator end() const { return _vertices.end(); }
+
     typename vertices_map_t::iterator begin() { return _vertices.begin(); }
     typename vertices_map_t::iterator end() { return _vertices.end(); }
 
