8b553e34688f7c7194917a21c1ba421b328d3fd5
Patryk Nowak <nowak.p92@gmail.com>
2019-08-20 21:44:57 +0200

    Add unit tests for the Graph class

--- graphs/test/graph/GraphTest.cpp
+++ graphs/test/graph/GraphTest.cpp
@@ -1,13 +1,68 @@
 #include <gtest/gtest.h>
+#include "gmock/gmock-matchers.h"
+#include "Graph.hpp"
+
+using ::testing::Contains;
+using ::testing::Key;
 
 
-TEST(GraphTest, DummyTest1)
+TEST(GraphTest, CreateEmptyGraph)
 {
-    EXPECT_EQ(2, 1+1);
+    Graph<> gr;
+    EXPECT_EQ(gr.begin(), gr.end());
 }
 
-TEST(GraphTest, DummyTest2)
+TEST(GraphTest, AddUniqueVertices)
 {
-    EXPECT_NE(3, 1+1);
+    Graph<> gr;
+    gr.addVertice(-5);
+    gr.addVertice(0);
+    gr.addVertice(5);
+
+    EXPECT_EQ(3, std::distance(gr.begin(), gr.end()));
+}
+
+TEST(GraphTest, AddDuplicateVertices)
+{
+    Graph<> gr;
+    gr.addVertice(5);
+    gr.addVertice(5);
+
+    EXPECT_EQ(1, std::distance(gr.begin(), gr.end()));
+    EXPECT_THAT(gr, Contains(Key(5)));
+}
+
+TEST(GraphTest, DeleteVertice)
+{
+    Graph<> gr;
+    gr.addVertice(1);
+    gr.addVertice(2);
+    gr.addVertice(3);
+
+    EXPECT_EQ(3, std::distance(gr.begin(), gr.end()));
+    gr.deleteVertice(2);
+    EXPECT_EQ(2, std::distance(gr.begin(), gr.end()));
+}
+
+TEST(GraphTest, DeleteNonExistingVertice)
+{
+    Graph<> gr;
+    gr.addVertice(1);
+    gr.addVertice(2);
+    gr.addVertice(3);
+
+    EXPECT_EQ(3, std::distance(gr.begin(), gr.end()));
+    gr.deleteVertice(15);
+    EXPECT_EQ(3, std::distance(gr.begin(), gr.end()));
+}
+
+TEST(GraphTest, ClearGraph)
+{
+    Graph<> gr;
+    gr.addVertice(1);
+
+    EXPECT_NE(gr.begin(), gr.end());
+    gr.clear();
+    EXPECT_EQ(gr.begin(), gr.end());
 }
 
