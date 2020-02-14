2df45e37216bfeaa1f90cc23206a14892764b96b
Patryk Nowak <nowak.p92@gmail.com>
2020-02-14 21:23:22 +0100

    Adapt Graph2DSample to use ArgumentConverter

--- apps/Graph2DSample/src/main.cpp
+++ apps/Graph2DSample/src/main.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
 #include <thread>
-#include <chrono>
-#include <array>
 #include <AlgoUI/Window.hpp>
+#include <CommandParser/ArgumentConverter.hpp>
 #include <CommandParser/CommandParser.hpp>
 #include "DrawableGraph.hpp"
 
@@ -11,6 +10,7 @@ using Vertice = DrawableGraph::Point;
 using UDGraph = algo::ui::UpdatableDrawable<DrawableGraph>;
 using algo::ui::Window;
 using algo::cmd::CommandParser;
+using algo::cmd::ArgumentConverter;
 
 
 void parseInput(std::shared_ptr<UDGraph> dg)
@@ -21,13 +21,10 @@ void parseInput(std::shared_ptr<UDGraph> dg)
         {
             "addVertice", [&](std::istringstream& args)
             {
-                Vertice v;
-                args >> v.x >> v.y >> v.id;
-                if (!args)
-                {
-                    std::cout << "invalid parameters" << std::endl;
-                    return;
-                }
+                ArgumentConverter<int, int, char> ac;
+                auto tuple = ac.convert(args);
+                Vertice v { std::get<0>(tuple), std::get<1>(tuple), std::get<2>(tuple) };
+
                 gr.addVertice(v);
                 dg->update(gr);
                 std::cout << "addVertice complete" << std::endl;
@@ -36,15 +33,14 @@ void parseInput(std::shared_ptr<UDGraph> dg)
         {
             "addEdge", [&](std::istringstream& args)
             {
+                ArgumentConverter<char, char, int> ac;
+                auto tuple = ac.convert(args);
+
                 Vertice from, to;
-                int value;
-                args >> from.id >> to.id >> value;
-                if (!args)
-                {
-                    std::cout << "invalid parameters" << std::endl;
-                    return;
-                }
-                gr.addEdge(from, to, value);
+                from.id = std::get<0>(tuple);
+                to.id = std::get<1>(tuple);
+
+                gr.addEdge(from, to, std::get<2>(tuple));
                 dg->update(gr);
                 std::cout << "addEdge complete" << std::endl;
             }
@@ -52,13 +48,12 @@ void parseInput(std::shared_ptr<UDGraph> dg)
         {
             "deleteVertice", [&](std::istringstream& args)
             {
+                ArgumentConverter<char> ac;
+                auto tuple = ac.convert(args);
+
                 Vertice v;
-                args >> v.id;
-                if (!args)
-                {
-                    std::cout << "invalid parameters" << std::endl;
-                    return;
-                }
+                v.id = std::get<0>(tuple);
+
                 gr.deleteVertice(v);
                 dg->update(gr);
                 std::cout << "deleteVertice complete" << std::endl;
@@ -67,13 +62,13 @@ void parseInput(std::shared_ptr<UDGraph> dg)
         {
             "deleteEdge", [&](std::istringstream& args)
             {
+                ArgumentConverter<char, char> ac;
+                auto tuple = ac.convert(args);
+
                 Vertice from, to;
-                args >> from.id >> to.id;
-                if (!args)
-                {
-                    std::cout << "invalid parameters" << std::endl;
-                    return;
-                }
+                from.id = std::get<0>(tuple);
+                to.id = std::get<1>(tuple);
+
                 gr.deleteEdge(from, to);
                 dg->update(gr);
                 std::cout << "deleteEdge complete" << std::endl;
